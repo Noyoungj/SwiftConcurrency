4fbd338f9f48653a16f1ab7820142a0fde5cf33f
noyoungj96 <yj.no14@konai.com>
2024-04-24 14:40:16 +0900

    Initial Commit

---
+++ SwiftCurrency/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftCurrency
+//
+//  Created by 노영재(Youngjae No)_인턴 on 4/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SwiftCurrencyUITests/SwiftCurrencyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftCurrencyUITestsLaunchTests.swift
+//  SwiftCurrencyUITests
+//
+//  Created by 노영재(Youngjae No)_인턴 on 4/24/24.
+//
+
+import XCTest
+
+final class SwiftCurrencyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
