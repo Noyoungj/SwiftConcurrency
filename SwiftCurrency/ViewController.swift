ba787b89a117cf54a28860c731b294cb0d52c223
noyoungj96 <yj.no14@konai.com>
2024-04-25 16:33:07 +0900

    Feat: URL 연동

--- SwiftCurrency/ViewController.swift
+++ SwiftCurrency/ViewController.swift
@@ -15,20 +15,21 @@ class ViewController: UIViewController {
         $0.backgroundColor = .blue
     }
     
-    private let imageView = UIImageView().then{
-        $0.contentMode = .scaleAspectFill
+    private let label = UILabel().then{
+        $0.font = UIFont.systemFont(ofSize: 15)
+        $0.textColor = .black
+        $0.textAlignment = .center
     }
     
     private func addSubviews() {
         self.view.addSubview(button)
-        self.view.addSubview(imageView)
+        self.view.addSubview(label)
     }
     
     private func setLayouts() {
-        imageView.snp.makeConstraints{
+        label.snp.makeConstraints{
             $0.leading
                 .trailing
-                .top
                 .equalTo(view.safeAreaLayoutGuide)
             $0.bottom
                 .equalTo(view.safeAreaLayoutGuide.snp.centerY)
@@ -46,21 +47,53 @@ class ViewController: UIViewController {
     
     @objc
     private func clickAction(_ sender: UIButton) {
-        guard let url = URL(string: "https://reqres.in/api/users?page=2") else { return }
-        
+        guard let url = URL(string: "https://auth.cryptokonai.com/realms/cryptokona/protocol/openid-connect/token") else { return }
+        guard let ethurl = URL(string: "https://gateway.cryptokonai.com/eth/getPrice") else { return }
+
         Task {
-            guard let imageURL = try? await self.requestImageURL(requestURL: url),
-                  let url = URL(string: imageURL),
-                  let data = try? Data(contentsOf: url) else { return }
+            guard let token = try? await self.requestAccessURL(requestURL: url) else { return }
             print("is clickAction MainThread?: \(Thread.isMainThread)")
-            self.imageView.image = UIImage(data: data)
+            guard let price = try? await self.requestGetETHURL(requestURL: ethurl, accessToken: token) else { return }
+            self.label.text = String(price)
         }
     }
+    func requestGetETHURL(requestURL: URL, accessToken: String) async throws -> Float {
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "GET"
+        
+        request.setValue("Bearer \(accessToken)", forHTTPHeaderField: "Authorization")
+        
+        let (data, _) = try await URLSession.shared.data(for: request)
+        return try JSONDecoder().decode(GetETHModel.self, from: data).data?.price ?? 0
+    }
     
-    func requestImageURL(requestURL: URL) async throws -> String {
+    func requestAccessURL(requestURL: URL) async throws -> String {
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        
+        let parameters : [String: String] = ["client_id": "cryptokona_api",
+                                          "client_secret": "LyiPUL0NKyTPuocOZXOLdkvTjlHBdDH0",
+                                          "scope": "openid",
+                                          "grant_type": "client_credentials"]
+        // 파라미터를 URLQueryItem으로 변환
+        let queryItems = parameters.map { (key, value) in
+            return URLQueryItem(name: key, value: value)
+        }
+
+        // URLComponents를 사용하여 쿼리 스트링으로 변환
+        var urlComponents = URLComponents()
+        urlComponents.queryItems = queryItems
+
+        // URLRequest의 httpBody에 설정
+        request.httpBody = urlComponents.query?.data(using: .utf8)
+        
         print("is requestImageURL MainThread?: \(Thread.isMainThread)")
-        let (data, _) = try await URLSession.shared.data(from: requestURL)
-        return try JSONDecoder().decode(MyModel.self, from: data).data.first?.avatar ?? ""
+        let (data, response) = try await URLSession.shared.data(for: request)
+        if let responseData = String(data: data, encoding: .utf8) {
+            print("Server error message:", responseData)
+        }
+        return try JSONDecoder().decode(AccessTokenModel.self, from: data).access_token ?? ""
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -74,34 +107,15 @@ class ViewController: UIViewController {
 
 }
 
-struct MyModel: Codable {
-    let page, perPage, total, totalPages: Int
-    let data: [Datum]
-    let support: Support
-
-    enum CodingKeys: String, CodingKey {
-        case page
-        case perPage = "per_page"
-        case total
-        case totalPages = "total_pages"
-        case data, support
-    }
+struct AccessTokenModel: Codable {
+    let access_token: String?
 }
 
-struct Datum: Codable {
-    let id: Int
-    let email, firstName, lastName: String
-    let avatar: String
-
-    enum CodingKeys: String, CodingKey {
-        case id, email
-        case firstName = "first_name"
-        case lastName = "last_name"
-        case avatar
-    }
+struct GetETHModel: Codable {
+    let data: EthModel?
 }
 
-struct Support: Codable {
-    let url: String
-    let text: String
+struct EthModel: Codable {
+    let price: Float?
 }
+
